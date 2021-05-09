cc179e3bd7025472b5b3f3b47d3ded843044b448
Noha Hassan <69641077+nohahassan64@users.noreply.github.com>
2021-05-10 02:49:20 +0300

    Update index.js

--- src/components/HeoSection/index.js
+++ src/components/HeoSection/index.js
@@ -16,7 +16,7 @@ const HeroSection = () => {
                 <HeroItems>
                     <HeroH1>Greatest Pizza Ever</HeroH1>
                     <HeroP>Ready in 60 seconds</HeroP>
-                    <HeroBtn>Place Order</HeroBtn>
+                    <HeroBtn>Place Orderkkkkkkkkkkkkkkkkkkkkk</HeroBtn>
                 </HeroItems>
             </HeroContent>   
         </HeroContainer>
